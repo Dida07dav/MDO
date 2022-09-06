bf69711d2c6991d62e59095dcdfee87c42501c2d
Dida <davidova1981@bk.ru>
2022-09-06 22:06:24 +0300

    dop

--- dp.py
+++ dp.py
@@ -1,67 +1,107 @@
 # Давыдова Лидия Викторовна
 #  Python 225
 # Задание14
+#
+# from math import pi
+#
+#
+# def square(figure_type, **kwargs):
+# 	if figure_type == "rhombus":
+# 		return kwargs["d1"] * kwargs["d2"] / 2
+# 	if figure_type == "square":
+# 		return kwargs["a"] ** 2
+# 	if figure_type == "trapezoid":
+# 		return (kwargs["a"] + kwargs["b"]) * (1 / 2) * kwargs["h"]
+# 	if figure_type == "circle":
+# 		return pi * kwargs["r"] ** 2
+# 	return "invalid data"
+#
+#
+# print(square(figure_type="rhombus", d1=10, d2=8))
+# print(square(figure_type="square", a=5))
+# print(square(figure_type="trapezoid", a=12, b=3, h=6))
+# print(square(figure_type="circle", r=18))
+# print(square(figure_type="unknown", a=1, b=2, c=3))
+#
+# # Задание 15
+#
+# print((lambda x, y: x * y * 5)(2, 5))
+#
+# # или
+#
+# summa = lambda a=2, b=5, c=5: a * b * c
+# print(summa())
+#
+# # №2
+#
+# students = [
+# 	{'name': 'Jennifer', 'final': 95},
+# 	{'name': 'David', 'final': 92},
+# 	{'name': 'Nikolas', 'final': 98},
+# ]
+# res1 = sorted(students, key=lambda std: std['name'])
+# print(res1)
+# res2 = sorted(students, key=lambda std: std['final'], reverse=True)
+# print(res2)
+#
+# # №3
+#
+# students = [
+# 	{'name': 'Jennifer', 'final': 95},
+# 	{'name': 'David', 'final': 92},
+# 	{'name': 'Nikolas', 'final': 98},
+# ]
+# res1 = max(students, key=lambda std: std['name'])
+# print(res1)
+# res2 = min(students, key=lambda std: std['name'])
+# print(res2)
+#
+# # №4
+#
+# nums = [3, 5, 7, 3, 9, 5, 7, 2]
+# summa1 = list(map(lambda x: x**2, nums))
+# summa2 = list(map(lambda x: x**3, nums))
+# print(summa1)
+# print(summa2)
+#
 
-from math import pi
+# Задание 17
+# I am learning Python. hello, WORLD!
+# # 1
+# s = input("-> ")
+# a = s.find("h")
+# b = s.rfind("h")
+# print(s[:a] + s[b + 1:])
+#
+#
+# # 2
+#
+# # s = "I am learning Python. hello, WORLD!"
+# s = input("-> ")
+# a = s[:s.find("h")]
+# b = s[s.find("h"):s.rfind("h") + 1]
+# c = s[s.rfind("h") + 1:]
+# s = a + b[::-1] + c
+# print(s)
 
+# 3
 
-def square(figure_type, **kwargs):
-	if figure_type == "rhombus":
-		return kwargs["d1"] * kwargs["d2"] / 2
-	if figure_type == "square":
-		return kwargs["a"] ** 2
-	if figure_type == "trapezoid":
-		return (kwargs["a"] + kwargs["b"]) * (1 / 2) * kwargs["h"]
-	if figure_type == "circle":
-		return pi * kwargs["r"] ** 2
-	return "invalid data"
+# s = input("Строка: ")
+# s_replace = input("Ее заменяемая строка: ")
+# s_new = input("Новая подстрока: ")
+#
+# a = s.find(s_replace)
+# while a != -1:
+# 	b = len(s_replace)
+# 	s = s[:a] + s_new + s[a+b:]
+# 	a = s.find(s_replace)
+# print(s)
 
-
-print(square(figure_type="rhombus", d1=10, d2=8))
-print(square(figure_type="square", a=5))
-print(square(figure_type="trapezoid", a=12, b=3, h=6))
-print(square(figure_type="circle", r=18))
-print(square(figure_type="unknown", a=1, b=2, c=3))
-
-# Задание 15
-
-print((lambda x, y: x * y * 5)(2, 5))
-
-# или
-
-summa = lambda a=2, b=5, c=5: a * b * c
-print(summa())
-
-# №2
-
-students = [
-	{'name': 'Jennifer', 'final': 95},
-	{'name': 'David', 'final': 92},
-	{'name': 'Nikolas', 'final': 98},
-]
-res1 = sorted(students, key=lambda std: std['name'])
-print(res1)
-res2 = sorted(students, key=lambda std: std['final'], reverse=True)
-print(res2)
-
-# №3
-
-students = [
-	{'name': 'Jennifer', 'final': 95},
-	{'name': 'David', 'final': 92},
-	{'name': 'Nikolas', 'final': 98},
-]
-res1 = max(students, key=lambda std: std['name'])
-print(res1)
-res2 = min(students, key=lambda std: std['name'])
-print(res2)
-
-# №4
-
-nums = [3, 5, 7, 3, 9, 5, 7, 2]
-summa1 = list(map(lambda x: x**2, nums))
-summa2 = list(map(lambda x: x**3, nums))
-print(summa1)
-print(summa2)
-
+# 4
+#
+# text = " Ежевику для ежат \n Принесли два ежа,\n Ежевику еле-еле \n Ежата возле ели съели."
+# str = (' ' + text).lower().count(' е')
+# print(text, "(", str, "слов", ")")
+# print()
+# print("Количество слов: ", str)
 
