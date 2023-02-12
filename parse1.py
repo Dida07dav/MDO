9dbb3003ee266cb8a07ee2da86ba8a15f32aa548
Dida <davidova1981@bk.ru>
2023-02-12 21:59:38 +0300

    dp

--- parse1.py
+++ parse1.py
@@ -20,19 +20,21 @@ class Parser:
 			title = item.find("h3").text
 			href = item.find("a").get('href')
 			author = item.find("a", class_="topic-info-author-link").text.strip()
+			description = item.find("div", class_="topic-content text").text.strip()
 			self.res.append({
 				"title": title,
 				"href": href,
-				"author": author
+				"author": author,
+				"description": description,
 			})
-	
+			
 	def server(self):
 		with open(self.path1, "w") as f:
 			i = 1
 			for item in self.res:
-				f.write(f'Кино и сериалы №{i}\n\nНазвание: {item["title"]}\n'
-						f'Ссылка: {item["href"]}\nАвтор: {item["author"]}'
-						f'\n\n{"*" * 25}\n')
+				f.write(f'Кино и сериалы №{i}\n\nНазвание: {item["title"]}.\n'
+						f'Описание:{item["description"]}\nАвтор: {item["author"]}'
+						f'\nСсылка: {item["href"]}\n\n{"*" * 25}\n')
 				i += 1
 	
 	def run(self):
